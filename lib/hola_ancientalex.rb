4850c9db86f909b9cd706596c63466266a6f10c8
David Hahn <dhahn@ctatechs.com>
2012-10-31 22:26:25 -0500

    added documentation

--- lib/hola_ancientalex.rb
+++ lib/hola_ancientalex.rb
@@ -1,4 +1,13 @@
+# The main Hola driver
 class Hola
+# Say hi to the world!
+
+# Example:
+# 	>> Hola.hi(:spanish)
+# 	=>
+#
+# Arguments:
+#		language: (Symbol)
 	def self.hi(language = :english)
 		translator = Translator.new(language)
 		translator.hi
